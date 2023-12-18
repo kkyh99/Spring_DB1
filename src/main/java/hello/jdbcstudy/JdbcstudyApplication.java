a052d5fee156b9da6ecc9f99225f8395a464c8ca
kimyeonho <dksk87721@gmail.com>
2023-12-19 03:44:26 +0900

    프로젝트 생성

---
+++ src/main/java/hello/jdbcstudy/JdbcstudyApplication.java
@@ -0,0 +1,13 @@
+package hello.jdbcstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/jdbcstudy/JdbcstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.jdbcstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
