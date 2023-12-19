ae89ec711285f80c4b89cbd54e0375734f9475d4
kimyeonho <dksk87721@gmail.com>
2023-12-19 22:36:34 +0900

    트랜잭션 AOP 적용(@Transactional) - 트랜잭션 관리 코드를 비지니스 로직에서 제거

---
+++ src/main/java/hello/jdbcstudy/service/MemberServiceV3_3.java
@@ -0,0 +1,40 @@
+package hello.jdbcstudy.service;
+
+import hello.jdbcstudy.domain.Member;
+import hello.jdbcstudy.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
+@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceV3_3 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+            bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromMember.getMemberId(), fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toMember.getMemberId(), toMember.getMoney() + money);
+    }
+
+
+    private void validation(Member toMember){
+        if(toMember.getMemberId().equals("ex")){
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
